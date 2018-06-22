6e81b48511c8deec805d38a2fbe261649e230ebd
E.B <eduwht2@gmail.com>
2018-06-22 17:25:29 +0200

    Add files via upload

---
+++ BD/resetBD.sql
@@ -0,0 +1,16 @@
+use f1;
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE circuits;
+TRUNCATE escuderies;
+TRUNCATE bolids;
+TRUNCATE contractes;
+TRUNCATE treballadors;
+TRUNCATE pilots;
+TRUNCATE mecanics;
+TRUNCATE provadors;
+TRUNCATE parts_bolid;
+TRUNCATE responsables;
+TRUNCATE valoracions;
+
+SET FOREIGN_KEY_CHECKS = 1;
